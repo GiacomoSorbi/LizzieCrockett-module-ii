f1acd0f875b1858fb4df3f9c34cc89edc40dd7e7
Lizzie <elizabethsiancrockett@gmail.com>
2020-06-30 16:46:14 +0100

    Updates

---
+++ precache-manifest.19484ba3a0ec353dbc22e08fc887a1a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a52aba6901b768175967a1927b7cc7b",
+    "url": "/LizzieCrockett-module-ii/index.html"
+  },
+  {
+    "revision": "f051379e9380060b93fe",
+    "url": "/LizzieCrockett-module-ii/static/css/main.343e3416.chunk.css"
+  },
+  {
+    "revision": "a13751c49d0e65df2b48",
+    "url": "/LizzieCrockett-module-ii/static/js/2.db3f6bf9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LizzieCrockett-module-ii/static/js/2.db3f6bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f051379e9380060b93fe",
+    "url": "/LizzieCrockett-module-ii/static/js/main.bdbfea16.chunk.js"
+  },
+  {
+    "revision": "ce5c132d8697d839e6c2",
+    "url": "/LizzieCrockett-module-ii/static/js/runtime-main.29407199.js"
+  },
+  {
+    "revision": "5f1bb3260936947de0b3fd3d39c43caa",
+    "url": "/LizzieCrockett-module-ii/static/media/histories.5f1bb326.png"
+  },
+  {
+    "revision": "296b497067f38a61d8d4f2b61d01cf57",
+    "url": "/LizzieCrockett-module-ii/static/media/lelyportrait.296b4970.jpg"
+  }
+]);
