b9235743f107fb1df801cb34c4c7054d3bf989a0
Lizzie <elizabethsiancrockett@gmail.com>
2020-07-01 15:09:13 +0100

    various typos corrected

--- src/constants/afteraphra-content.js
+++ src/constants/afteraphra-content.js
@@ -8,7 +8,7 @@ const afterAphraContent = [
     {
         title: 'Women writers after Aphra',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod maximus ultricies. Phasellus rhoncus tellus quis massa auctor condimentum. Vestibulum eu massa felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla odio urna, tempor lacinia blandit a, posuere quis leo. Sed et fermentum nibh. Maecenas pharetra, elit eu sodales consequat, nunc lectus faucibus lectus, a sagittis neque dui eget velit. Aenean pulvinar purus velit, quis lobortis arcu pretium at. Ut ac leo varius, faucibus sem eget, porta enim. Phasellus in tellus leo. Nulla et ante consectetur, fringilla libero non, bibendum nisl. Integer blandit dapibus iaculis. In quam lectus, luctus consequat urna sit amet, ornare luctus lectus. Nunc facilisis turpis nec varius condimentum. Cras vitae pharetra nibh. Curabitur neque nisl.',
-        image: 'The Bronte Sisters',
+        image: 'The Bronte sisters',
         source: 'https://raw.githubusercontent.com/GiacomoSorbi/LizzieCrockett-module-ii/master/src/images/brontesisters.jpg'
     },
     {
@@ -23,12 +23,6 @@ const afterAphraContent = [
         image: 'Aphra Behn reconsidered',
         source: 'https://raw.githubusercontent.com/GiacomoSorbi/LizzieCrockett-module-ii/master/src/images/aphralinedrawing.png'
     },
-    {
-        title: 'Fifth story',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod maximus ultricies. Phasellus rhoncus tellus quis massa auctor condimentum. Vestibulum eu massa felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla odio urna, tempor lacinia blandit a, posuere quis leo. Sed et fermentum nibh. Maecenas pharetra, elit eu sodales consequat, nunc lectus faucibus lectus, a sagittis neque dui eget velit. Aenean pulvinar purus velit, quis lobortis arcu pretium at. Ut ac leo varius, faucibus sem eget, porta enim. Phasellus in tellus leo. Nulla et ante consectetur, fringilla libero non, bibendum nisl. Integer blandit dapibus iaculis. In quam lectus, luctus consequat urna sit amet, ornare luctus lectus. Nunc facilisis turpis nec varius condimentum. Cras vitae pharetra nibh. Curabitur neque nisl.',
-        image: 'Image caption',
-        source: { histories }
-    }
 ]
 
 export default afterAphraContent;
